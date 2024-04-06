f3461703d244b9d3ae9707fdabb1bf7633ec4fdf
Minn Ong <s10207192@connect.np.edu.sg>
2024-04-07 00:17:50 +0800

    Created EVCharging Class and XML, added onClickListener in MainActivity

---
+++ app/src/main/java/com/example/sparks2/EVChargingActivity.java
@@ -0,0 +1,16 @@
+package com.example.sparks2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EVChargingActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ev_charging);
+
+        // Any additional initialization or setup for the EV Charging activity can be done here
+    }
+}
--- app/src/main/java/com/example/sparks2/MainActivity.java
+++ app/src/main/java/com/example/sparks2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sparks2;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -17,6 +18,23 @@ import com.example.sparks2.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView evIcon = findViewById(R.id.imageView7);
+        evIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the EV Charging activity
+                Intent intent = new Intent(MainActivity.this, EVChargingActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
