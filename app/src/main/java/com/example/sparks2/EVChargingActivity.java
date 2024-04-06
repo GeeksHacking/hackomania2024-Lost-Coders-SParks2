33118103d7e79b8352db7248dcd91c57f56d6cad
Minn Ong <s10207192@connect.np.edu.sg>
2024-04-07 02:19:47 +0800

    Added Toolbar, RecyclerView, and Buttons for EVCharging Activity

--- app/src/main/java/com/example/sparks2/EVChargingActivity.java
+++ app/src/main/java/com/example/sparks2/EVChargingActivity.java
@@ -1,8 +1,11 @@
 package com.example.sparks2;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 
 public class EVChargingActivity  extends AppCompatActivity {
 
@@ -11,6 +14,23 @@ public class EVChargingActivity  extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ev_charging);
 
-        // Any additional initialization or setup for the EV Charging activity can be done here
+        Toolbar toolbar = findViewById(R.id.evChargingHeader);
+        setSupportActionBar(toolbar);
+
+        // Set title/header name
+        getSupportActionBar().setTitle("EV Charging");
+
+        // Enable the back button in the toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Set a click listener on the back button
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle back button click
+                onBackPressed();
+            }
+        });
     }
+
 }
